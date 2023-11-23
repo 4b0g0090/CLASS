19f8a2621a58bc6e775d24c5ddc2ff631abb2a11
4b0g0090 <112836287+4b0g0090@users.noreply.github.com>
2023-11-23 10:08:59 +0800

    Create CLASS_SCORE.py

---
+++ CLASS_SCORE.py
@@ -0,0 +1,21 @@
+class score:
+    def __init__(self):
+        self.math='80'
+        self.chinese='70'
+        self.physical='75'
+        self.english='85'
+        self.geography='83'
+    
+    def score1(self):
+        print("my math score:",self.math)
+    
+
+    def vote(self):
+        if self.chinese>80:
+            print("you like god")
+        else:
+            print("you loss")
+
+obj=score()
+obj.score1()
+
